a41219966ba65ef939b10f811b67ec007fbcb0f6
rarojas <raul.rojaschattin@cencosud.cl>
2016-12-09 13:46:46 -0300

    cambio de variables restarWas

--- lib/restartWas.sh
+++ lib/restartWas.sh
@@ -1,12 +1,12 @@
-ruta="/u01/IBM/WebSphere/AppServer/profiles/AppSrv01/bin/stopServer.sh"
-ruta2="/u01/IBM/WebSphere/AppServer/profiles/AppSrv01/bin/startServer.sh"
-insta="server1"
-rutaSP="/u01/IBM/WebSphere/AppServer/profiles/AppSrv01/logs/server1/server1.pid"
-rtemp="/u01/IBM/WebSphere/AppServer/profiles/AppSrv01/temp/*"
-rtran="/u01/IBM/WebSphere/AppServer/profiles/AppSrv01/tranlog/*"
-rwste="/u01/IBM/WebSphere/AppServer/profiles/AppSrv01/wstemp/*"
-user="wasadmin"
-pass="wasadmin1"
+ruta=`cat lib/configuracion.txt | awk '/ruta=/ && !/awk/ {print $2}'`
+ruta2=`cat lib/configuracion.txt | awk '/ruta2=/ && !/awk/ {print $2}'`
+insta=`cat lib/configuracion.txt | awk '/insta=/ && !/awk/ {print $2}'`
+rutaSP=`cat lib/configuracion.txt | awk '/rutaSP=/ && !/awk/ {print $2}'`
+rtemp=`cat lib/configuracion.txt | awk '/rtemp=/ && !/awk/ {print $2}'`
+rtran=`cat lib/configuracion.txt | awk '/rtran=/ && !/awk/ {print $2}'`
+rwste=`cat lib/configuracion.txt | awk '/rwste=/ && !/awk/ {print $2}'`
+user=`cat lib/configuracion.txt | awk '/user=/ && !/awk/ {print $2}'`
+pass=`cat lib/configuracion.txt | awk '/pass=/ && !/awk/ {print $2}'`
 
 stopW()
 {
@@ -25,6 +25,18 @@ stopW()
 					echo "El pid no concuerda con el servicio a detener, favor de detenerlo de manera manual"
 			fi
 	else
+			appservice=`ps -fea | grep $insta | egrep -v grep | awk '{print $2}'`
+			var=${#appservice}
+			if (( $var  > 2  ));
+			then
+					echo `$ruta $insta "-username" $user "-password" $pass `
+					echo `rm -rf $rtemp`
+					echo `rm -rf $rwste`
+					echo `rm -rf $rtran`
+					echo "El servicio se a detenido, eliminado el contenido de las carpetas temp, tranlog y wstemp"
+			else
+					echo "El proceso no existe."
+			fi
 			echo "El servicio ya se encuentra detenido"
 	fi
 }
